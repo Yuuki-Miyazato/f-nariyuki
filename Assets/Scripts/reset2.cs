59f5f7a61091f8f5c65c82cc288661cd738ebb8d
Yuuki-Miyazato <65996576+Yuuki-Miyazato@users.noreply.github.com>
2021-04-08 16:02:21 +0900

    Add files via upload

---
+++ Assets/Scripts/reset2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class reset2 : MonoBehaviour
+{
+    [SerializeField] GameObject Key;
+    [SerializeField] Keyh k;
+    string sceneName;
+
+    void Start()
+    {
+        Key = GameObject.Find("Key");
+        k = Key.GetComponent<Keyh>();
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (k.keyflg == true)
+            {
+                SceneManager.LoadScene("Enemymap");
+            }
+        }
+    }
+}
