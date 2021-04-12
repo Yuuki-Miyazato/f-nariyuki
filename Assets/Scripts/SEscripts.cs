f545b152abb53f67fd7b618e16c84cc4c2c8db4b
naakazyasu <65997000+naakazyasu@users.noreply.github.com>
2021-04-12 16:32:53 +0900

    プレイヤーの足音です

---
+++ Assets/Scripts/SEscripts.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEscripts : MonoBehaviour
+{
+    public AudioClip sound1;
+
+    AudioSource audioSource;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Component取得
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+      // 左
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            audioSource.Play();
+            
+        }
+        // 右
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            audioSource.Play();
+            
+        }
+        // 上
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            audioSource.Play();
+           
+        }
+        // 下
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            audioSource.Play();
+        }
+    }
+}
