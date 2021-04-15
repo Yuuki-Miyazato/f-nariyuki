45aaceb1d8bd622e3dc3e9dcb749264da674df3e
masashi <masashi-takayasu-19f@stu.kbc.ac.jp>
2021-04-16 00:43:48 +0900

    過去の画像消去

--- Assets/Scripts/reset2.cs
+++ Assets/Scripts/reset2.cs
@@ -25,59 +25,47 @@ public class reset2 : MonoBehaviour
         k = Key.GetComponent<Keyh>();
         sceneName = SceneManager.GetActiveScene().name;
     }
-<<<<<<< HEAD
     void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-=======
-
-    private void Update()
-    {
-        if (abc == false)
-        {
-            waitTime += 0.1f;
-            if (waitTime >= 10)
+            private void Update()
             {
-                SceneManager.LoadScene("Title");
-            }
-        }
-        Debug.Log(waitTime);
-    }
-
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-
-        
-        if (collision.gameObject.tag == "Player")
-        {
-            
-
-
->>>>>>> 487e0f02ad4849d8002e35002a37e300633fafe2
-            if (k.keyflg == true)
-            {
-                Instantiate(clear);
-                clear.SetActive(true);
-
-                if (abc)
+                if (abc == false)
                 {
-                    //ゴール音を１度だけ鳴らす
-                    abc = false;
-                    AudioSource.PlayClipAtPoint(sound01, transform.position);
+                    waitTime += 0.1f;
+                    if (waitTime >= 10)
+                    {
+                        SceneManager.LoadScene("Title");
+                    }
                 }
-<<<<<<< HEAD
-                Invoke("Return",3);
-=======
+                Debug.Log(waitTime);
+            }
 
->>>>>>> 487e0f02ad4849d8002e35002a37e300633fafe2
+
+            void OnTriggerEnter2D(Collider2D collision)
+            {
+
+
+                if (collision.gameObject.tag == "Player")
+                {
+                    if (k.keyflg == true)
+                    {
+                        Instantiate(clear);
+                        clear.SetActive(true);
+
+                        if (abc)
+                        {
+                            //ゴール音を１度だけ鳴らす
+                            abc = false;
+                            AudioSource.PlayClipAtPoint(sound01, transform.position);
+                        }
+                        Invoke("Return", 3);
+                    }
+                }
+            }
+            void Return()
+            {
+                SceneManager.LoadScene("Enemymap2");
             }
         }
-        //Debug.Log(waitTime);
-    }
-    void Return()
-    {
-        SceneManager.LoadScene("Enemymap2");
-    }
-}
--- Assets/Scripts/reset3.cs
+++ Assets/Scripts/reset3.cs
@@ -25,10 +25,7 @@ public class reset3 : MonoBehaviour
         k = Key.GetComponent<Keyh>();
         sceneName = SceneManager.GetActiveScene().name;
     }
-
-<<<<<<< HEAD
     void OnTriggerStay2D(Collider2D collision)
-=======
     private void Update()
     {
         if (abc ==false)
@@ -43,34 +40,21 @@ public class reset3 : MonoBehaviour
     }
 
     void OnTriggerEnter2D(Collider2D collision)
->>>>>>> 487e0f02ad4849d8002e35002a37e300633fafe2
     {
         if (collision.gameObject.tag == "Player")
         {
-<<<<<<< HEAD
-=======
-
->>>>>>> 487e0f02ad4849d8002e35002a37e300633fafe2
             if (k.keyflg == true)
             {
                 Instantiate(clear);
                 clear.SetActive(true);
-<<<<<<< HEAD
-
-=======
->>>>>>> 487e0f02ad4849d8002e35002a37e300633fafe2
                 if (abc)
                 {
                     //ゴール音を１度だけ鳴らす
                     abc = false;
                     AudioSource.PlayClipAtPoint(sound01, transform.position);
                 }
-<<<<<<< HEAD
                 Invoke("Return",3);
-=======
-
->>>>>>> 487e0f02ad4849d8002e35002a37e300633fafe2
-            }
+         }
         }
     }
     void Return()
