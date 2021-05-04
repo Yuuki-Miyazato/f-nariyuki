362c6fb2cf48dc182f117ba51b3abe2ca3afc4df
masashi <masashi-takayasu-19f@stu.kbc.ac.jp>
2021-05-04 16:17:04 +0900

    ゲームオーバー時にゲームを終了できるようにした

---
+++ Assets/Scripts/exit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exit : MonoBehaviour
+{
+    
+    public void OnStartButtonClicked()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        UnityEngine.Application.Quit();
+    }
+}
