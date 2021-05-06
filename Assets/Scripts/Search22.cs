7624e52eeb7765abbf87342a092f88cc4a7d785b
Yuuki-Miyazato <65996576+Yuuki-Miyazato@users.noreply.github.com>
2021-05-07 00:37:17 +0900

    追加

---
+++ Assets/Scripts/Search22.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Search22 : MonoBehaviour
+{
+    public  GameObject Key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Key = GameObject.Find("Key");                     //Playerという名前のオブジェクトを探しPlayerに入れる
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = Camera.main.WorldToScreenPoint(transform.localPosition);
+        var rotation = Quaternion.LookRotation(Vector3.forward, Key.transform.position - transform.position);
+        transform.localRotation = rotation;
+    }
+}
---
+++ Assets/Scripts/search12.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class search12 : MonoBehaviour
+{
+    public GameObject Goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Goal = GameObject.Find("Goal");                     //Playerという名前のオブジェクトを探しPlayerに入れる
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = Camera.main.WorldToScreenPoint(transform.localPosition);
+        var rotation = Quaternion.LookRotation(Vector3.forward, Goal.transform.position - transform.position);
+        transform.localRotation = rotation;
+    }
+}
