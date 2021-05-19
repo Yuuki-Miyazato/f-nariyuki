16343623f764d4b58469f48a1afd6fb558d3207d
Yuuki-Miyazato <65996576+Yuuki-Miyazato@users.noreply.github.com>
2021-05-19 10:49:26 +0900

    Add files via upload

--- Assets/Scripts/Keyh.cs
+++ Assets/Scripts/Keyh.cs
@@ -12,18 +12,22 @@ public class Keyh : MonoBehaviour
     private bool abc = true;
 
     //public Sprite chest;
-    //SpriteRenderer MainSpriteRenderer;
+    SpriteRenderer MainSpriteRenderer;
+
+    public Animator anim;
 
     [SerializeField] public GameObject effect;
+    [SerializeField] private GameObject Goal;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         keyflg = false;
-        //MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         GameObject effectobj = Instantiate(effect, this.transform.position, Quaternion.identity);
         effectobj.transform.parent = transform;
         effectobj.name = "T";
-
+        Goal = GameObject.FindWithTag("goal");
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -36,6 +40,12 @@ public class Keyh : MonoBehaviour
                 //ゴール音を１度だけ鳴らす
                 abc = false;
                 AudioSource.PlayClipAtPoint(sound01, transform.position);
+                GameObject goaleffectDelet = GameObject.Find("goaleffect(Clone)");
+                Destroy(goaleffectDelet);
+                Goal.layer = 0;
+                anim.enabled = false;
+                MainSpriteRenderer.sprite = null;
+               // Destroy(gameObject);
             }
             keyflg = true;
            // MainSpriteRenderer.sprite = chest;
