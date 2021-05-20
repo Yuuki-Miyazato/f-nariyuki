f5bb46fc1629ec6007d68f4c6c2c064230ab0978
Yuuki-Miyazato <65996576+Yuuki-Miyazato@users.noreply.github.com>
2021-05-20 17:50:28 +0900

    Add files via upload

---
+++ Assets/Scripts/GameClearscene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameClearscene : MonoBehaviour
+{
+    void Return()
+    {
+        if (SceneManager.GetActiveScene().name == "GameClear")
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+}
